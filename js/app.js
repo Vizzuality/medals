a26d6bbb2e9b424c98234b3277f32ac1377ccc85
Javier Arce <javierarce@gmail.com>
2012-08-09 16:00:15 +0200

    Removes default zoom

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ CONFIG = {
   zoom: 4,
   query: "SELECT * FROM {{table_name}}",
   tileURL: 'http:///{s}.tile.stamen.com/toner/{z}/{x}/{y}.png',
-  mapOptions: {maxZoom: 18, attribution: "Powered by Leaflet and Mapbox"},
+  mapOptions: { maxZoom: 18, attribution: "Powered by Leaflet and Mapbox", zoomControl: false},
   styles: {
     gdp: "#london_2012_olympic_ { point-file: url(/home/ubuntu/tile_assets/viz2/blueDot2.svg); point-allow-overlap:true; text-face-name: 'DejaVu Sans Bold'; text-fill:#000; text-size:10; text-halo-fill:rgba(255,255,255,1); text-halo-radius:0; text-line-spacing:1; text-wrap-width:20; text-opacity:.7; text-allow-overlap:true; text-name:'[iso]'; line-width:1.3; } " +
       "#london_2012_olympic_ [total_gdp <= 121] { point-transform:'scale(2.2)'; } " +
@@ -69,8 +69,8 @@ function zoomEnd(e) {
 function init() {
 
   // Create the maps
-  maps.actual = new L.Map('actual').setView(CONFIG.center, CONFIG.zoom);
-  maps.gdp    = new L.Map('gdp').setView(CONFIG.center, CONFIG.zoom);
+  maps.actual = new L.Map('actual', { zoomControl: false }).setView(CONFIG.center, CONFIG.zoom);
+  maps.gdp    = new L.Map('gdp',    { zoomControl: false }).setView(CONFIG.center, CONFIG.zoom);
 
   // Layers configuration
   var layers = {
